eb6d3f2a9205a95df7e4d92c405b2a30ea85a7d7
Yoshiko Hatanaka <yohatana@c6r2s1.42tokyo.jp>
2025-06-12 15:14:34 +0900

    add validation infile format

--- srcs/validate_infile.c
+++ srcs/validate_infile.c
@@ -1,20 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   validate_map.c                                     :+:      :+:    :+:   */
+/*   validate_infile.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yohatana <yohatana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 11:57:54 by yohatana          #+#    #+#             */
-/*   Updated: 2025/06/12 14:21:22 by yohatana         ###   ########.fr       */
+/*   Updated: 2025/06/12 15:13:07 by yohatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
-
-static bool	is_texture(char *line);
-static bool	is_roof_floor(char *line);
-static bool	is_map(char *line);
 
 /*
 1. ファイル名が.cub
@@ -31,81 +27,16 @@ void	validate_infile(t_line **head)
 	validate_line_format(head);
 }
 
-void	validate_infile_format(t_line **head)
+int	count_line_list(t_line **head)
 {
 	t_line	*curr;
-	int		texture_count;
-	int		color_count;
-	int		map_count;
+	int		count;
 
-	curr = *head;
-	texture_count = 0;
+	count = 0;
 	while (curr)
 	{
-		if (is_texture(curr->str))
-		{
-			while (is_texture(curr->str))
-			{
-				texture_count++;
-			}
-			// 4つ連続して存在する
-		}
+		count++;
 		curr = curr->next;
 	}
-}
-
-static bool	is_texture(char *line)
-{
-	if (ft_strncmp("NO", line, 2) == 0 || \
-		ft_strncmp("SO", line, 2) == 0 || \
-		ft_strncmp("EA", line, 2) == 0 || \
-		ft_strncmp("WE", line, 2) == 0)
-	{
-		printf("%s", line);
-		return (true);
-	}
-	return (false);
-}
-
-static bool	is_roof_floor(char *line)
-{
-	if (ft_strncmp("F", line, 1) == 0 || ft_strncmp("C", line, 1) == 0)
-	{
-		printf("%s", line);
-		return (true);
-	}
-	return (false);
-}
-
-static bool	is_map(char *line)
-{
-	int	i;
-
-	i = 0;
-	while (line[i])
-	{
-		while (line[i] == ' ')
-			i++;
-		if (line[i] == '1')
-			return (true);
-		i++;
-	}
-	return (false);
-}
-
-// return 1 is error
-bool	validate_txture(t_line *line)
-{
-	int		i;
-	char	*path;
-
-	/*
-		空白で分割されていること
-		パスが有効なこと
-		同じ先頭２文字がないこと
-	*/
-	// ４つ連続で記載されていること
-	if (line->str[2] != ' ')
-		return (true);
-	return (false);
+	return (count);
 }
---
+++ srcs/validate_infile_format.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validate_infile_format.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yohatana <yohatana@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 15:09:25 by yohatana          #+#    #+#             */
+/*   Updated: 2025/06/12 15:13:48 by yohatana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	*get_line_info(t_line **head);
+
+enum	line_type {
+	is_empty,
+	texture,
+	color,
+	map
+};
+
+void	validate_infile_format(t_line **head)
+{
+	t_line	*curr;
+	int		line_count;
+	int		*line_info;
+	int		index;
+
+	curr = *head;
+	index = 0;
+	line_count = count_line_list(head);
+	line_info = get_line_info(head);
+	// 要素が連続していること
+	// 要素が必要以上に存在しないこと
+	// mapが最後に記載されていること
+}
+
+static int	*get_line_info(t_line **head)
+{
+	t_line	*curr;
+	int		line_count;
+	int		*line_info;
+	int		index;
+
+	curr = *head;
+	line_info = (int *)ft_calloc(sizeof(int), line_count);
+	if (!line_info)
+	{
+		free_line_list(head);
+		exit_error("");
+	}
+	while (curr)
+	{
+		if (is_texture(curr->str))
+			line_info[index] = texture;
+		if (is_color(curr->str))
+			line_info[index] = color;
+		if (is_map(curr->str))
+			line_info[index] = map;
+		if (ft_strcmp("\n", curr->str) == 0)
+			line_info[index] = is_empty;
+		curr = curr->next;
+		index++;
+	}
+	return (line_info);
+}
