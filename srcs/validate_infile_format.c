a68730cba1b79333e949e028321e041a005d074f
Yoshiko Hatanaka <yohatana@c6r3s5.42tokyo.jp>
2025-06-16 17:06:20 +0900

    fix for rev

--- srcs/validate_infile_format.c
+++ srcs/validate_infile_format.c
@@ -6,7 +6,7 @@
 /*   By: yohatana <yohatana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 15:09:25 by yohatana          #+#    #+#             */
-/*   Updated: 2025/06/15 17:12:02 by yohatana         ###   ########.fr       */
+/*   Updated: 2025/06/16 17:03:15 by yohatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,8 @@ void	validate_infile_format(t_line **head)
 	pre_line = NULL;
 	while (curr)
 	{
+		if (ft_strcmp(curr->str, "\n") == 0)
+			curr = curr->next;
 		if (is_texture(curr->str))
 			curr = validate_texture_block(curr, pre_line, head);
 		else if (is_color(curr->str))
@@ -43,10 +45,9 @@ void	validate_infile_format(t_line **head)
 				return ;
 		}
 		pre_line = curr;
-		if (ft_strcmp(curr->str, "\n") == 0)
-			curr = curr->next;
-		else if (!is_texture(curr->str) && \
-				!is_color(curr->str) && !is_map(curr->str))
+		if (!is_texture(curr->str) && \
+			!is_color(curr->str) && !is_map(curr->str) && \
+			ft_strcmp(curr->str, "\n") != 0)
 			exit_error_infile_format("", head);
 	}
 }
@@ -61,19 +62,19 @@ static t_line	*validate_texture_block(t_line *curr, \
 	watch_line = curr;
 	count = 0;
 	if (pre != NULL && ft_strcmp(pre->str, "\n") != 0)
-		exit_error_infile_format("element split empty line", head);
+		exit_error_infile_format("Separate elements with empty lines", head);
 	while (is_texture(watch_line->str))
 	{
 		watch_line = watch_line->next;
 		count++;
 	}
 	if (count != 4)
-		exit_error_infile_format("texture is 4 line", head);
+		exit_error_infile_format("texture must consist of 4 lines", head);
 	curr = watch_line;
 	while (watch_line)
 	{
 		if (is_texture(watch_line->str))
-			exit_error_infile_format("too many texture line", head);
+			exit_error_infile_format("too many texture lines", head);
 		watch_line = watch_line->next;
 	}
 	return (curr);
@@ -89,19 +90,19 @@ static t_line	*validate_color_block(t_line *curr, \
 	watch_line = curr;
 	count = 0;
 	if (pre != NULL && ft_strcmp(pre->str, "\n") != 0)
-		exit_error_infile_format("element split empty line", head);
+		exit_error_infile_format("separate elements with empty lines", head);
 	while (is_color(watch_line->str))
 	{
 		watch_line = watch_line->next;
 		count++;
 	}
 	if (count != 2)
-		exit_error_infile_format("color is 2 line", head);
+		exit_error_infile_format("color must consist of 4 lines", head);
 	curr = watch_line;
 	while (watch_line)
 	{
 		if (is_color(watch_line->str))
-			exit_error_infile_format("too many texture line", head);
+			exit_error_infile_format("too many color lines", head);
 		watch_line = watch_line->next;
 	}
 	return (curr);
@@ -115,7 +116,7 @@ static t_line	*validate_map_block(t_line *curr, \
 
 	watch_line = curr;
 	if (pre != NULL && ft_strcmp(pre->str, "\n") != 0)
-		exit_error_infile_format("element split empty line", head);
+		exit_error_infile_format("separate elements with empty lines", head);
 	while (is_map(watch_line->str))
 	{
 		watch_line = watch_line->next;
@@ -124,7 +125,7 @@ static t_line	*validate_map_block(t_line *curr, \
 	{
 		if (ft_strcmp(watch_line->str, "\n") != 0 && \
 		ft_strcmp(watch_line->str, "") != 0)
-			exit_error_infile_format("map is last element", head);
+			exit_error_infile_format("map must be the last element.", head);
 		watch_line = watch_line->next;
 	}
 	curr = watch_line;
@@ -134,8 +135,8 @@ static t_line	*validate_map_block(t_line *curr, \
 static void	exit_error_infile_format(char *str, t_line **head)
 {
 	free_line_list(head);
-	write(2, "Error\ninvalid map: ", 20);
-	write(2, str, ft_strlen(str));
-	write(2, "\n", 1);
+	ft_putendl_fd("Error", 2);
+	ft_putstr_fd("invalid map: ", 2);
+	ft_putendl_fd(str, 2);
 	exit(1);
 }
