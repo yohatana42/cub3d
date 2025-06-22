30b6446f006434d812f2d146440b4dc09c70d67c
yuzu-juice <yuzujuicesec@gmail.com>
2025-06-22 03:29:43 +0000

    add: free textures

--- srcs/clean_up.c
+++ srcs/clean_up.c
@@ -3,18 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   clean_up.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yohatana <yohatana@student.42.fr>          +#+  +:+       +#+        */
+/*   By: takitaga <takitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 14:15:26 by yohatana          #+#    #+#             */
-/*   Updated: 2025/06/21 16:45:17 by yohatana         ###   ########.fr       */
+/*   Updated: 2025/06/22 03:29:03 by takitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
+static void	destroy_textures(t_mlx_data *mlx_data);
+
 int	clean_up(t_mlx_data *mlx_data)
 {
+	destroy_textures(mlx_data);
 	clean_map_data(mlx_data->map_data);
 	close_window(mlx_data);
 	return (0);
+}
+
+static void	destroy_textures(t_mlx_data *mlx_data)
+{
+	mlx_destroy_image(mlx_data->mlx, mlx_data->north_tex.img.img);
+	mlx_destroy_image(mlx_data->mlx, mlx_data->south_tex.img.img);
+	mlx_destroy_image(mlx_data->mlx, mlx_data->west_tex.img.img);
+	mlx_destroy_image(mlx_data->mlx, mlx_data->east_tex.img.img);
 }
